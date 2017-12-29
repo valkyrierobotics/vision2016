6b2b2d643fe267a3007063bbee3849165e91fe79
Amit Palekar <amit12.palekar@gmail.com>
2017-12-29 14:23:37 -0800

    Fix kernel condition for gaussian blur

--- y2017/filters/gaussianBlur.cpp
+++ y2017/filters/gaussianBlur.cpp
@@ -1,8 +1,8 @@
 #include "filters/gaussianBlur.hpp"
 
-void gaussianBlur(cv::Mat &img, int blur_ksize, int sigmaX, int sigmaY) 
+void gaussianBlur(cv::Mat &img, int blur_ksize, int sigmaX, int sigmaY)
 {
-	if (((blur_ksize % 2) == 0) || (blur_ksize < 0)) // If kernel size is even or negative, round to nearest upper odd number
+	if (((blur_ksize % 2) == 0) && (blur_ksize > 0)) // Kernel size must be positive and odd
 		blur_ksize++;
 
 	cv::GaussianBlur(img, img, cv::Size(blur_ksize, blur_ksize), sigmaX, sigmaY, cv::BORDER_DEFAULT);
